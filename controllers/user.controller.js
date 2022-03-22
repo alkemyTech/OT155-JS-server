262c71952ec90031df5b89db2aceb17e1900850f
mcdzo <maximilianocardozo710@gmail.com>
2022-03-22 16:18:47 -0300

    Correct errors on password validation in register controller (password is not encrypted yet)

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,12 +1,11 @@
 const validator = require("validator");
-const User  = require("../models/user");
+const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const controller = {
   login: (req, res) => {
     const params = req.body;
-    console.log(params)
 
     try {
       const validate_email = validator.isEmpty(params.email);
@@ -25,52 +24,50 @@ const controller = {
         where: {
           email: params.email,
         },
-      }).then((result) => {
-        if (result === null) {
-          console.log("There is no user registered with that email.");
-          return res.status(200).send({
-            message: "There is no user registered with that email.",
-            value: false,
-          });
-        }
+      })
+        .then((result) => {
+          if (result === null) {
+            console.log("There is no user registered with that email.");
+            return res.status(200).send({
+              message: "There is no user registered with that email.",
+              value: false,
+            });
+          }
 
-        //The user exists so check the password
-        console.log(params.password + "" + result.password)
-        console.log(bcrypt.compareSync(params.password, result.password))
-        if (!bcrypt.compareSync(params.password, result.password)) {
-          //incorrect password
-          console.log("Incorrect password");
+          //note: check encrypted password    if(!bcrypt.compareSync(params.password, result.password))
+
+          if (params.password !== result.password) {
+            console.log("Incorrect password");
+            return res.status(200).send({
+              status: "invalid password",
+              value: false,
+            });
+          } else {
+            const token = jwt.sign(
+              { id: result.id, email: result.email },
+              process.env.TOKEN_KEY,
+              {
+                expiresIn: "2d",
+              }
+            );
+            result.token = token;
+          }
+          const user = {
+            id: result.id,
+            name: result.name,
+            surname: result.surname,
+            email: result.email,
+          };
           return res.status(200).send({
-            status: "invalid password",
-            value: false,
+            status: "success",
+            value: true,
+            user: user,
+            jwt: result.token,
           });
-        } else {
-          //generate jwt
-          const token = jwt.sign(
-            { id: result.id, email: result.email },
-            process.env.TOKEN_KEY,
-            {
-              expiresIn: "2d",
-            }
-          );
-          result.token = token;
-        }
-        const user = {
-          id: result.id,
-          name: result.name,
-          surname: result.surname,
-          email: result.email,
-        };
-        return res.status(200).send({
-          status: "success",
-          value: true,
-          user: user,
-          jwt: result.token,
+        })
+        .catch((err) => {
+          return console.log(err);
         });
-      })
-      .catch(err=> {
-          return console.log(err)
-      })
     } catch (err) {
       console.log(err);
       return res.status(500).send({
@@ -78,7 +75,6 @@ const controller = {
       });
     }
   },
- 
 };
 
 module.exports = controller;
