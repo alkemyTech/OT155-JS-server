cdf4ba04dd2a299671b9e9decb4d2662208f968e
MurabitoF <francomurabito@outlook.com>
2022-03-22 10:13:41 -0300

    Constraint added

--- migrations/20220320220159-create-slide.js
+++ migrations/20220320220159-create-slide.js
@@ -1,39 +1,43 @@
-'use strict';
+"use strict";
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Slides', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      imageUrl: {
-        type: Sequelize.STRING
-      },
-      text: {
-        type: Sequelize.STRING
-      },
-      order: {
-        type: Sequelize.INTEGER
-      },
-      organizationId: {
-        type: Sequelize.INTEGER
-      },
-      deleteAt: {
-        type: Sequelize.DATE
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Slides');
-  }
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable("Slides", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			imageUrl: {
+				type: Sequelize.STRING,
+				allowNull: true,
+			},
+			text: {
+				type: Sequelize.STRING,
+				allowNull: true,
+			},
+			order: {
+				type: Sequelize.INTEGER,
+				allowNull: true,
+			},
+			organizationId: {
+				type: Sequelize.INTEGER,
+				allowNull: true,
+			},
+			deleteAt: {
+				type: Sequelize.DATE,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable("Slides");
+	},
 };
