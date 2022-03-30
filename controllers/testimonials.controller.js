01d6775aff5eb37192cd647c71df0193e6b67f2d
adrianlunazzi <adrianlunazzi@mail.com>
2022-03-30 16:22:48 -0300

    modify code at testimonial controller

--- controllers/testimonials.controller.js
+++ controllers/testimonials.controller.js
@@ -3,16 +3,22 @@ const db = require("../models");
 const testimonials = {
   createTestimonial: async (req, res) => {
     const { name, image, content } = req.body;
-    await db.Testimonials.create({
-      name,
-      image,
-      content,
-    }).then(function (data) {
-      return res.status(200).json({
-        status: "Success",
-        testimonial: data,
+    try {
+      await db.Testimonials.create({
+        name,
+        image,
+        content,
+      }).then(function (data) {
+        return res.status(200).json({
+          status: "Success",
+          testimonial: data,
+        });
       });
-    });
+    } catch (error) {
+      res.status(400).json({
+        message: "(!) Something has gone wrong. Check the entries",
+      });
+    }
   },
 };
 
