11743595c4eaee4b356b160cb68704182e769fbf
Daniel Campos <danielgcampos.04@gmail.com>
2022-03-31 15:31:34 -0300

    created controllers getNew, add try catch in controllers news

--- controllers/news.controllers.js
+++ controllers/news.controllers.js
@@ -1,19 +1,42 @@
-const News = require('../models/new.js')
+const {DataTypes} = require('sequelize')
+const db = require('../models/')
+const News = require('../models/new.js')(db.sequelize,DataTypes)
+
 
 const getNews = async (req,res) => {
-    const news = await News.findAll({
-        order:['createdAt'],
-        limit:10
-    })
-    // map news
-    news = news.map(({title,image,createdAt}) => ({
-        name:title,
-        image,
-        createdAt
-    }))
-    res.json(news)
+    try{
+        const news = await News.findAll({
+            order:['createdAt'],
+            limit:10
+        })
+
+        // map news
+        news = news.map(({title,image,createdAt}) => ({
+            name:title,
+            image,
+            createdAt
+        }))
+        res.json(news)
+    }catch(e) {
+        console.log(e)
+        return res.status(500).json({message:'(!) Something has gone wrong'})
+    }
+}
+
+const getNew = async (req, res) => {
+    try{
+        const newActivity = await findByPk(req.params.newId)
+        return res.json({
+            status:'success',
+            value:true,
+            new:newActivity
+        })
+    }catch{
+        return res.status(500).json({message:'(!) Something has gone wrong'})
+    }
 }
 
 module.exports = {
-    getNews
+    getNews,
+    getNew
 }
