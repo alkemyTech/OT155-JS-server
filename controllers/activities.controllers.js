82ce4ab3e6fa147568dabf0be32e4864dfc3a9b0
Daniel Campos <danielgcampos.04@gmail.com>
2022-04-04 15:58:43 -0300

    remove comments

--- controllers/activities.controllers.js
+++ controllers/activities.controllers.js
@@ -1,26 +1,26 @@
-// const Activities = require('../models/activities.js')
+const Activities = require('../models/activities.js')
 const validator = require("validator");
 
 const createActivitie = async (req,res) => {
-    // const {name,image,content} = req.body
+    const {name,image,content} = req.body
 
-    // try{
-    //     if(validator.isEmpty(name) || validator.isEmpty(content) ) 
-    //         return res.status(401).json({message: "Name and Content fields are required"})
+    try{
+        if(validator.isEmpty(name) || validator.isEmpty(content) ) 
+            return res.status(401).json({message: "Name and Content fields are required"})
         
-    //     const newActivities = await Activities.create({
-    //         title:name,
-    //         image,
-    //         content,
-    //     })
-    //     res.json({
-    //         status:'success',
-    //         value:true,
-    //         new:newActivities
-    //     })
-    // }catch{
-    //     res.status(500).json({message:'(!) Something has gone wrong. '})
-    // }
+        const newActivities = await Activities.create({
+            title:name,
+            image,
+            content,
+        })
+        res.json({
+            status:'success',
+            value:true,
+            new:newActivities
+        })
+    }catch{
+        res.status(500).json({message:'(!) Something has gone wrong. '})
+    }
 }
 
 
