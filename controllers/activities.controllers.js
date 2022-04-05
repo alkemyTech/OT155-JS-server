7d15d0fb651c3245adce1d42d5ab03485886e28b
mcdzo <maximilianocardozo710@gmail.com>
2022-04-05 16:20:31 -0300

    Add update-activity Route and Controller

--- controllers/activities.controllers.js
+++ controllers/activities.controllers.js
@@ -1,29 +1,89 @@
-const Activities = require('../models/activities.js')
+const {DataTypes} = require('sequelize')
+const db = require('../models/')
+const Activities = require("../models/activities.js")(db.sequelize,DataTypes);
 const validator = require("validator");
 
-const createActivitie = async (req,res) => {
-    const {name,image,content} = req.body
+const createActivitie = async (req, res) => {
+  const { name, image, content } = req.body;
 
-    try{
-        if(validator.isEmpty(name) || validator.isEmpty(content) ) 
-            return res.status(401).json({message: "Name and Content fields are required"})
-        
-        const newActivities = await Activities.create({
-            title:name,
-            image,
-            content,
-        })
-        res.json({
-            status:'success',
-            value:true,
-            new:newActivities
-        })
-    }catch{
-        res.status(500).json({message:'(!) Something has gone wrong. '})
+  try {
+    if (validator.isEmpty(name) || validator.isEmpty(content))
+      return res
+        .status(401)
+        .json({ message: "Name and Content fields are required" });
+
+    const newActivities = await Activities.create({
+      title: name,
+      image,
+      content,
+    });
+    res.json({
+      status: "success",
+      value: true,
+      new: newActivities,
+    });
+  } catch {
+    res.status(500).json({ message: "(!) Something has gone wrong. " });
+  }
+};
+
+const updateActivity = async (req, res) => {
+  const params = req.body;
+
+  try {
+    if (!params.id || !params.name || !params.image || !params.concept) {
+      return res.status(400).json({
+        ok: false,
+        message: "(!) Error. Missing fields.",
+      });
     }
-}
 
+    validate_id = validator.isEmpty(params.id);
+    validate_name = validator.isEmpty(params.name);
+    validate_image = validator.isEmpty(params.image);
+    validate_concept = validator.isEmpty(params.concept);
+
+    if (validate_id || validate_name || validate_image || validate_concept) {
+      return res.status(400).json({
+        ok: false,
+        message: "(!) Error. Some fields are empty.",
+      });
+    }
+
+    const activity = await Activities.findOne({where: {id: params.id}})
+
+    if(!activity){
+        return res.status(404).json({
+            ok: false,
+            message: "(!) Error. Activity not found.",
+          });
+    }
+
+    const updateActivity = await Activities.update(
+        { name: params.name, image: params.image, concept: params.concept },
+        { where: { id: params.id } }
+      );
+
+    const updatedActivity = await Activities.findOne({where: {id: params.id}})
+  
+      return res.status(200).json({
+        ok: true,
+        message: "Activity updated successfully",
+        updatedActivity
+       
+      });
+    
+  } catch (err) {
+      console.log(err)
+    return res.status(500).json({
+      ok: false,
+      message: "(!) Something's gone wrong. Try again.",
+      error: err,
+    });
+  }
+};
 
 module.exports = {
-    createActivitie
-}
+  createActivitie,
+  updateActivity
+};
--- routes/activities.js
+++ routes/activities.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const newsControllers = require("../controllers/activities.controllers.js")
 
 router.post('/activities', newsControllers.createActivitie)
+router.put('/update-activity', newsControllers.updateActivity)
 
 module.exports = router;
