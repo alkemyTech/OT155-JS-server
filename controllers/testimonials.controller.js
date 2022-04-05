0051ede853c98ce839f22e7d413ca95b74cc6b77
Adrian Lunazzi <74929359+adrianlunazzi@users.noreply.github.com>
2022-04-05 09:35:11 -0300

    Ot155 86 - Endpoint Delete -Testimonials (#18)
    * Create testimonial DELETE endpoint
    
    * syntax error fixed
    
    Co-authored-by: adrianlunazzi <adrianlunazzi@mail.com>

--- controllers/testimonials.controller.js
+++ controllers/testimonials.controller.js
@@ -24,6 +24,30 @@ const testimonialsMethods = {
       return errorResponse;
     }
   },
+  deleteTestimonial: async (req, res) => {
+    const { id } = req.params;
+    const testimonialExist = await Testimonials.findOne({
+      where: { id },
+    });
+    if (testimonialExist) {
+      await Testimonials.destroy({
+        where: {
+          id,
+        },
+      });
+      const successfullResponse = res.status(200).json({
+        status: "success",
+        value: true,
+        Message: `Testimonial ${testimonialExist.name} has been deleted`,
+      });
+    } else {
+      const errorResponse = res.status(400).json({
+        message:
+          "(!) There are not Testimonials registered with the params sent",
+      });
+      return errorResponse;
+    }
+  },
 };
 
 module.exports = testimonialsMethods;
