5daf845c39647266e2371012deb9524d56a24504
MurabitoF <francomurabito@outlook.com>
2022-04-05 14:33:10 -0300

    Created members endpoint

--- app.js
+++ app.js
@@ -11,9 +11,10 @@ const usersRouter = require("./routes/users");
 const organizationsRouter = require("./routes/organizations");
 const testimonialsRouter = require("./routes/testimonials");
 const activitiesRouter = require("./routes/activities");
-const newsRouter = require('./routes/news');
-const categoriesRouter = require('./routes/categories')
-const authRouter = require("./routes/auth")
+const newsRouter = require("./routes/news");
+const categoriesRouter = require("./routes/categories");
+const authRouter = require("./routes/auth");
+const membersRouter = require("./routes/members");
 
 const app = express();
 app.use(cors());
@@ -32,10 +33,11 @@ app.use("/api", indexRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/news", newsRouter);
 app.use("/api/organizations", organizationsRouter);
-app.use('/categories', categoriesRouter)
+app.use("/categories", categoriesRouter);
 app.use("/api/testimonials", testimonialsRouter);
 app.use("/api/activities", activitiesRouter);
-app.use("/api/auth", authRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/members", membersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
