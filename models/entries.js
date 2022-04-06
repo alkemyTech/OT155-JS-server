84702a4bbab92eef9c10e6d3c6eb8bea7a80ade3
Leonardo Farina <leonardoagustinfarina@gmail.com>
2022-04-06 10:55:40 -0300

    entries: models and migration update

--- models/entries.js
+++ models/entries.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Entries.belongsTo(models.Categories, { as: "category" });
     }
   }
   Entries.init({
