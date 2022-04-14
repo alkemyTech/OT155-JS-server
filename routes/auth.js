5c73345176bbf23284c44f0a29b127ddee7dde6f
unknown <luciabagnis@gmail.com>
2022-04-14 16:32:14 -0300

    add middleware

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller')
 const { validateJWT } = require('../Middlewares/validateJWT')
 
-router.get('/me', validateJWT,authController.getCurrentUser)
+router.get('/me', validateJWT ,authController.getCurrentUser)
 
 module.exports = router
