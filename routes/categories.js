c7578938ed91f54dd40b61e1537242e165c508e5
Leonardo Farina <leonardoagustinfarina@gmail.com>
2022-04-14 11:20:07 -0300

    Endpoint Creation Categories update

--- routes/categories.js
+++ routes/categories.js
@@ -7,8 +7,11 @@ const { validateFields } = require('../Middlewares/validateFields');
 router.delete("/delete/:id", categoriesController.deleteCategory)
 router.post("/",
 [
-    check('name', 'Name is required').not().isEmpty().trim().contains(''),
+    check('name', 'Name is required').not().isEmpty().trim(),
+    check('name', 'the name cannot be an empty field').trim().contains(''),
     check('name', 'The name must be a string').isString(),
+    check('description', 'description is required').not().isEmpty().trim(),
+    check('description', 'the description cannot be an empty field').trim().contains(''),
     validateFields
 ], categoriesController.postCategory)
 
