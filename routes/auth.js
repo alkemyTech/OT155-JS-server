bdb2b3d7cf991b9b56fde373a2de9d56a1e1b3c0
unknown <luciabagnis@gmail.com>
2022-04-14 16:28:55 -0300

    added validateJWT middleware to auth/me route

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router();
 const authController = require('../controllers/auth.controller')
+const { validateJWT } = require('../Middlewares/validateJWT')
 
-router.get('/me',authController.getCurrentUser)
+router.get('/me', validateJWT,authController.getCurrentUser)
 
 module.exports = router
