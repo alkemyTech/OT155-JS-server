8bd93713bcaa1fbcc36386987c5e0895581f55fa
Leonardo Farina <leonardoagustinfarina@gmail.com>
2022-04-14 11:03:19 -0300

    Endpoint Creation Categories added

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const categoriesController = require("../controllers/categories.controller")
-
+const { check } = require('express-validator');
+const { validateFields } = require('../Middlewares/validateFields');
 
 router.delete("/delete/:id", categoriesController.deleteCategory)
-// router.post("/", categoriesController.postCategory)
+router.post("/",
+[
+    check('name', 'Name is required').not().isEmpty().trim().contains(''),
+    check('name', 'The name must be a string').isString(),
+    validateFields
+], categoriesController.postCategory)
 
 module.exports = router
