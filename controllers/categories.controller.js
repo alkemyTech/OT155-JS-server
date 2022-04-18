124b9b5d4d00cdaf9c0e2361311c903644360bd7
unknown <luciabagnis@gmail.com>
2022-04-18 11:05:36 -0300

    get all categories endpoint

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,42 +1,50 @@
-const { param } = require('express-validator');
-const Categories = require("../models").Categories
+const { param } = require("express-validator");
+const db = require("../models");
+const {DataTypes} = require("sequelize")
+const Categories = require("../models/categories")(db.sequelize, DataTypes)
 
 const controller = {
-    deleteCategory: async(req,res) => {
+  getAllCategories: async (req, res) => {
+    try {
+      const categories = await Categories.findAll();
+      res.json({categories});
+    } catch (error) {
+      res.status(500).json({
+        message: "Something went wrong",
+      });
+    }
+  },
+  deleteCategory: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deleteCategory = await Categories.destroy({
+        where: {
+          id: id,
+        },
+      });
+    } catch (error) {
+      res.status(404).json({
+        msg: `Category does not exist`,
+      });
+    }
+  },
+  // postCategory: async(req,res) => {
+  //     const { name, description } = req.body;
+  //     try{
+  //         const newCategory = await Categories.create({
+  //             name,
+  //             description
+  //         })
+  //         res.status(201).json({
+  //             msg: `Category created successfully`,
+  //             newCategory
+  //         })
+  //     }catch(error){
+  //         res.status(500).json({
+  //             msg: `Something went wrong`
+  //         })
+  //     }
+  // }
+};
 
-        try{
-        const { id } = req.params;
-        const deleteCategory = await Categories.destroy({
-            where:{
-                id: id
-            }
-        })
-        }catch(error){
-            res.status(404).json({
-                msg: `Category does not exist`
-            })
-        }
-
-
-    },
-    // postCategory: async(req,res) => {
-    //     const { name, description } = req.body;
-    //     try{
-    //         const newCategory = await Categories.create({
-    //             name,
-    //             description
-    //         })
-    //         res.status(201).json({
-    //             msg: `Category created successfully`,
-    //             newCategory
-    //         })
-    //     }catch(error){
-    //         res.status(500).json({
-    //             msg: `Something went wrong`
-    //         })
-    //     }
-    // }
-}
-
-
-module.exports = controller
+module.exports = controller;
--- routes/categories.js
+++ routes/categories.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const categoriesController = require("../controllers/categories.controller")
 
-
+router.get("/", categoriesController.getAllCategories)
 router.delete("/delete/:id", categoriesController.deleteCategory)
 // router.post("/", categoriesController.postCategory)
 
