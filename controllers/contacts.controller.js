169f2a4d204d5f4a6d0151a3cc9eb9a540b66458
unknown <luciabagnis@gmail.com>
2022-04-18 10:47:52 -0300

    adds email service to contact post request

--- controllers/contacts.controller.js
+++ controllers/contacts.controller.js
@@ -1,4 +1,5 @@
 const Contact = require('../models').Contacts;
+const sendEmail = require("../services/mailService");
 
 const contactsPost = async (req, res) => {
   const { name, email } = req.body;
@@ -8,6 +9,11 @@ const contactsPost = async (req, res) => {
   try {
     Contact.build(contact);
     await contact.save();
+
+    const subject = "¡Muchas Gracias!";
+    const text = "Muchas gracias por comunicarte con Somos Más, estaremos en contacto a la brevedad"
+    sendEmail(email,subject,text)
+
     res.json({
       status: 'success',
       value: true,
