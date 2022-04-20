c958e2a2df765bd13314d5d4dab7f6d0d1759992
Adrian Lunazzi <74929359+adrianlunazzi@users.noreply.github.com>
2022-04-20 17:07:27 -0300

    add Endpoint POST - Members (#35)
    Co-authored-by: adrianlunazzi <adrianlunazzi@mail.com>
    Co-authored-by: Leonardo Farina <86262814+lfarina18@users.noreply.github.com>

--- routes/members.js
+++ routes/members.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
-const { getAllMembers, deleteMember } = require("../controllers/member.controller");
-const {validateJWT} = require("../Middlewares/validateJWT")
-const {authRole} = require("../auth/authRole") 
+const { createMembers, getAllMembers, deleteMember } = require("../controllers/member.controller");
+const {validateJWT} = require("../Middlewares/validateJWT");
+const {authRole} = require("../auth/authRole");
+const validateMembers = require("../Middlewares/validateMembers");
 
 router.get("/", getAllMembers);
-router.delete("/delete", validateJWT, authRole, deleteMember)
+router.delete("/delete", validateJWT, authRole, deleteMember);
+router.post("/", validateMembers, createMembers);
 
 module.exports = router;
