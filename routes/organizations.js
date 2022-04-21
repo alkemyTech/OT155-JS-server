2d893d21da3b04515453506d20c62620e73509e7
Leonardo Farina <leonardoagustinfarina@gmail.com>
2022-04-21 20:47:49 -0300

    Organization: edit migration, create seeder, create endpoint put

--- routes/organizations.js
+++ routes/organizations.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { getOrganizations } = require("../controllers/organizations.controller");
+const { getOrganizations, putOrganizations } = require("../controllers/organizations.controller");
 const router = express.Router();
 
 router.get("/:id/public", getOrganizations);
+router.put("/:id", putOrganizations);
 
 module.exports = router;
