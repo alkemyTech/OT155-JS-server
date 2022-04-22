974d96a7ccab4f123812476c43e051af09912422
Franco Murabito <66337385+MurabitoF@users.noreply.github.com>
2022-04-22 13:30:53 -0300

    Missing endpoint added (#53)

--- controllers/contacts.controller.js
+++ controllers/contacts.controller.js
@@ -1,32 +1,47 @@
-const Contact = require('../models').Contacts;
+const Contact = require("../models").Contacts;
 const sendEmail = require("../services/mailService");
 
-const contactsPost = async (req, res) => {
-  const { name, email } = req.body;
+const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await Contact.findAll({ where: { deletedAt: null } });
+    res.json({
+      status: "success",
+      value: true,
+      contacts,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "(!) Something has gone wrong. " });
+  }
+};
 
-  const contact = new Contact({ name, email });
+const contactsPost = async (req, res) => {
+  const { name, email, message } = req.body;
+  const phone = req.body.phone || null;
+  const contact = new Contact({ name, email, message, phone });
 
   try {
     Contact.build(contact);
     await contact.save();
 
     const subject = "¡Muchas Gracias!";
-    const text = "Muchas gracias por comunicarte con Somos Más, estaremos en contacto a la brevedad"
-    sendEmail(email,subject,text)
+    const text =
+      "Muchas gracias por comunicarte con Somos Más, estaremos en contacto a la brevedad";
+    sendEmail(email, subject, text);
 
     res.json({
-      status: 'success',
+      status: "success",
       value: true,
       contact,
     });
   } catch (error) {
     res.status(500).json({
-      message: 'Something went wrong',
+      message: "Something went wrong",
       value: false,
     });
   }
 };
 
 module.exports = {
-  contactsPost
+  getAllContacts,
+  contactsPost,
 };
