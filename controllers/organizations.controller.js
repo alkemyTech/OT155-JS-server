1063a6ca0629a8acd398963bedf29496980c93d8
Leonardo Farina <86262814+lfarina18@users.noreply.github.com>
2022-04-21 21:02:14 -0300

    Organization: edit migration, create seeder, create endpoint put (#51)

--- controllers/organizations.controller.js
+++ controllers/organizations.controller.js
@@ -6,11 +6,20 @@ const getOrganizations = async (req, res) => {
     const data = await Organizations.findByPk(id);
 
     if (data) {
-      const { name, image, phone, address, welcomeText, urlFacebook, urlLinkedin, urlInstagram } = data;
+      const {
+        name,
+        imageUrl,
+        phone,
+        address,
+        welcomeText,
+        urlFacebook,
+        urlLinkedin,
+        urlInstagram,
+      } = data;
 
       return res.json({
         name,
-        image,
+        imageUrl,
         phone,
         address,
         welcomeText,
@@ -20,16 +29,37 @@ const getOrganizations = async (req, res) => {
       });
     }
     res.status(404).json({
-     msg: `There is no organizations with the id: ${id}`,
+      msg: `There is no organizations with the id: ${id}`,
     });
-  }catch{
+  } catch {
     res.status(500).json({
-      msg:''
-    })
+      msg: 'Something is wrong, talk to the administrator',
+    });
   }
-  
+};
+
+const putOrganizations = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await Organizations.findByPk(id);
+    const { body } = req;
+
+    if (data) {
+      await data.update(body);
+
+      return res.json(data);
+    }
+    res.status(404).json({
+      msg: `There is no organizations with the id: ${id}`,
+    });
+  } catch {
+    res.status(500).json({
+      msg: 'Something is wrong, talk to the administrator',
+    });
+  }
 };
 
 module.exports = {
   getOrganizations,
+  putOrganizations,
 };
