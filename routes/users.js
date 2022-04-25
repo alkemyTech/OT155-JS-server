a06d8e77c32d2362b252547359c28819562d1e10
MurabitoF <francomurabito@outlook.com>
2022-04-25 12:20:27 -0300

    Validate JWT to get users

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/user.controller")
-const {authRole} = require("../auth/authRole")
-const {validateJWT} = require("../Middlewares/validateJWT")
+const userController = require("../controllers/user.controller");
+const { authRole } = require("../auth/authRole");
+const { validateJWT } = require("../Middlewares/validateJWT");
 
 /* GET users listing. */
-router.get('/',authRole,userController.getUser)
+router.get("/", validateJWT, authRole, userController.getUser);
 
+router.post("/login", userController.login);
+router.post("/register", userController.register);
 
-router.post("/login", userController.login)
-router.post("/register", userController.register)
-
-
-router.delete("/:id", validateJWT, userController.delete)
-router.put("/:id", userController.edit)
+router.delete("/:id", validateJWT, userController.delete);
+router.put("/:id", userController.edit);
 module.exports = router;
